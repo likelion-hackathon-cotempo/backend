f3b65aa3854f2d2569ea976d25d625354eeefa5f
박수민 <soomini030113@gmail.com>
2026-08-03 02:55:10 +0900

    [Feat] TeamMember 엔티티 구현

---
+++ src/main/java/cultureland/hackathon/team/entity/TeamMember.java
@@ -0,0 +1,45 @@
+package cultureland.hackathon.team.entity;
+
+import cultureland.hackathon.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(
+        name = "team_member",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_team_member",
+                columnNames = {"team_id", "member_id"}
+        )
+)
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TeamMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long teamMemberId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TeamRole role;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+    public static TeamMember create(TeamRole role, Member member, Team team) {
+        return TeamMember.builder()
+                .role(role)
+                .member(member)
+                .team(team)
+                .build();
+    }
+
+}
