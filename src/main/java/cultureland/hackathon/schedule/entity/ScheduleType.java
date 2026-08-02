e659d48afc7440a1b14d7252d1c1f849a76ce5a4
박수민 <soomini030113@gmail.com>
2026-08-03 02:57:16 +0900

    [Feat] ScheduleType enum 구현

---
+++ src/main/java/cultureland/hackathon/schedule/entity/ScheduleType.java
@@ -0,0 +1,7 @@
+package cultureland.hackathon.schedule.entity;
+
+public enum ScheduleType {
+    NATIONAL_HOLIDAY,   // 공식 공휴일
+    CULTURAL_BREAK,     // 체감 휴무 기간 (명절 전후, 유럽 8월 휴가철 등)
+    PERSONAL            // 개인 일정 (시험기간 등)
+}
