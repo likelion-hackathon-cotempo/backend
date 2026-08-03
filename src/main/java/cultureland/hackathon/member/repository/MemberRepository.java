4f22520bcd8c32ded35ff715ef12c800939b9b91
박수민 <soomini030113@gmail.com>
2026-08-03 12:49:54 +0900

    [Feat] MemberRepository 구현

---
+++ src/main/java/cultureland/hackathon/member/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package cultureland.hackathon.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.lang.reflect.Member;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
