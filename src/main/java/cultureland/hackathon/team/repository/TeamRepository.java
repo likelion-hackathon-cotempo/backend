d0ab9de2b4c3460a13f5a568f3738779641a9e7b
박수민 <soomini030113@gmail.com>
2026-08-04 16:49:33 +0900

    [Feat] TeamRepository 구현

---
+++ src/main/java/cultureland/hackathon/team/repository/TeamRepository.java
@@ -0,0 +1,14 @@
+package cultureland.hackathon.team.repository;
+
+import cultureland.hackathon.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByInviteCode(String inviteCode);
+
+    boolean existsByInviteCode(String inviteCode);
+
+}
