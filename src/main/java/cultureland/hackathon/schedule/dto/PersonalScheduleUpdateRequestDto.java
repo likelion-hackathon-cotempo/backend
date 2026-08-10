bb4d231f1980a00e571d132315f997bd0077e965
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-08-10 16:09:17 +0900

    [Feat] 개인일정 수정 요청 dto 구현

---
+++ src/main/java/cultureland/hackathon/schedule/dto/PersonalScheduleUpdateRequestDto.java
@@ -0,0 +1,27 @@
+package cultureland.hackathon.schedule.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Getter
+@NoArgsConstructor
+public class PersonalScheduleUpdateRequestDto {
+
+    @Size(max = 100, message = "Title must not exceed 100 characters.")
+    @Pattern(
+            regexp = ".*\\S.*",
+            message = "Title must not be blank."
+    )
+    private String title;
+
+    private Instant startDateTime;
+
+    private Instant endDateTime;
+
+    @Min(value = 1, message = "Weight must be between 1 and 3.")
+    @Max(value = 3, message = "Weight must be between 1 and 3.")
+    private Integer weight;
+}
